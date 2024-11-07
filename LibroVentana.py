fe8e952b48256f491b59f4b61452a4bb7a2afede
abrilmarolo <abrilmarolo@davinci.edu.ar>
2024-11-07 02:00:10 -0300

    Version Final

--- LibroVentana.py
+++ LibroVentana.py
@@ -109,46 +109,47 @@ def update_book_list(book_list):
 def open_book_crud(root, open_main_window):
     root.destroy()
     book_window = tk.Toplevel()
-    ancho_ventana = 400
-    alto_ventana = 700
+    ancho_ventana = 600
+    alto_ventana = 750
     x_ventana = book_window.winfo_screenwidth() // 2 - ancho_ventana // 2
     y_ventana = book_window.winfo_screenheight() // 2 - alto_ventana // 2
     posicion = str(ancho_ventana) + "x" + str(alto_ventana) + "+" + str(x_ventana) + "+" + str(y_ventana)
     book_window.geometry(posicion)
     book_window.title("Gestión de libros")
+    book_window.configure(bg="#f8f8f8")
     # book_window.geometry("400x700")
 
-    tk.Label(book_window, text="Titulo:").pack(pady=5)
-    title_entry = tk.Entry(book_window, width=30)
+    tk.Label(book_window, text="Titulo:", font=("Arial", 14), bg="#f8f8f8").pack(pady=5)
+    title_entry = tk.Entry(book_window, width=30, font=("Arial", 12), bg="#e5e5e5")
     title_entry.pack(pady=5)
 
-    tk.Label(book_window, text="Autor:").pack(pady=5)
-    author_entry = tk.Entry(book_window, width=30)
+    tk.Label(book_window, text="Autor:", font=("Arial", 14), bg="#f8f8f8").pack(pady=5)
+    author_entry = tk.Entry(book_window, width=30, font=("Arial", 12), bg="#e5e5e5")
     author_entry.pack(pady=5)
 
-    tk.Label(book_window, text="Precio:").pack(pady=5)
-    price_entry = tk.Entry(book_window, width=30)
+    tk.Label(book_window, text="Precio:", font=("Arial", 14), bg="#f8f8f8").pack(pady=5)
+    price_entry = tk.Entry(book_window, width=30, font=("Arial", 12), bg="#e5e5e5")
     price_entry.pack(pady=5)
 
-    tk.Label(book_window, text="Cantidad:").pack(pady=5)
-    quantity_entry = tk.Entry(book_window, width=30)
+    tk.Label(book_window, text="Cantidad:", font=("Arial", 14), bg="#f8f8f8").pack(pady=5)
+    quantity_entry = tk.Entry(book_window, width=30, font=("Arial", 12), bg="#e5e5e5")
     quantity_entry.pack(pady=5)
 
-    tk.Label(book_window, text="ID de categoria:").pack(pady=5)
-    category_entry = tk.Entry(book_window, width=30)
+    tk.Label(book_window, text="ID de categoria:", font=("Arial", 14), bg="#f8f8f8").pack(pady=5)
+    category_entry = tk.Entry(book_window, width=30, font=("Arial", 12), bg="#e5e5e5")
     category_entry.pack(pady=5)
 
-    tk.Button(book_window, text="Agregar libro", command=lambda: add_book(title_entry, author_entry, price_entry, quantity_entry, category_entry, book_list)).pack(pady=5)
+    tk.Button(book_window, text="Agregar libro", command=lambda: add_book(title_entry, author_entry, price_entry, quantity_entry, category_entry, book_list), bg="#d3d3d3",  font=("Arial", 12),activebackground= "#b5b5b5").pack(pady=5)
 
-    book_list = tk.Listbox(book_window, width=60, height=10)
-    book_list.pack(pady=10)
+    book_list = tk.Listbox(book_window, width=90, height=10)
+    book_list.pack(pady=5)
 
-    tk.Button(book_window, text="Actualizar libro seleccionado", command=lambda: update_book(book_list)).pack(pady=5)
-    tk.Button(book_window, text="Eliminar libro seleccionado", command=lambda: delete_book(book_list)).pack(pady=5)
+    tk.Button(book_window, text="Actualizar libro seleccionado", command=lambda: update_book(book_list), bg="#d3d3d3",  font=("Arial", 12),activebackground= "#b5b5b5").pack(pady=5)
+    tk.Button(book_window, text="Eliminar libro seleccionado", command=lambda: delete_book(book_list), bg="#d3d3d3",  font=("Arial", 12),activebackground= "#b5b5b5").pack(pady=5)
 
     update_book_list(book_list)
 
-    tk.Button(book_window, text="Volver", command=lambda: go_back(book_window, open_main_window)).pack(pady=10)
+    tk.Button(book_window, text="VOLVER",width=10, command=lambda: go_back(book_window, open_main_window), bg="#84001c", fg="#F8F8F8", font=("Arial", 10,"bold"),activebackground= "#6c0017",activeforeground="#eeeeee").pack(pady=10)
 
 def go_back(current_window, open_main_window):
     current_window.destroy()
--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from VentaVentana import open_sales_crud
 
 def open_main_window():
     root = tk.Tk()
-    ancho_ventana = 500
+    ancho_ventana = 600
     alto_ventana = 400
     x_ventana = root.winfo_screenwidth() // 2 - ancho_ventana // 2
     y_ventana = root.winfo_screenheight() // 2 - alto_ventana // 2
@@ -18,16 +18,16 @@ def open_main_window():
     main_label = tk.Label(root, text="Yenny", font=("Times New Roman", 35, "bold italic"), bg="#f8f8f8")
     main_label.pack(pady=20)
 
-    btn_libros = tk.Button(root, text="Libros", width=25, command=lambda: open_book_crud(root, open_main_window), font=("Arial", 15), bg="#034033", fg="#F8F8F8")
+    btn_libros = tk.Button(root, text="Libros", width=25, command=lambda: open_book_crud(root, open_main_window), font=("Arial", 15), bg="#034033", fg="#F8F8F8",activebackground= "#022f25", activeforeground="#F8F8F8")
     btn_libros.pack(pady=5)
 
-    btn_categorias = tk.Button(root, text="Categorias", width=25, command=lambda: open_category_crud(root, open_main_window), font=("Arial", 15), bg="#034033", fg="#F8F8F8")
+    btn_categorias = tk.Button(root, text="Categorias", width=25, command=lambda: open_category_crud(root, open_main_window), font=("Arial", 15), bg="#034033", fg="#F8F8F8",activebackground= "#022f25", activeforeground="#F8F8F8")
     btn_categorias.pack(pady=5)
 
-    btn_ventas = tk.Button(root, text="Ventas", width=25, command=lambda: open_sales_crud(root, open_main_window), font=("Arial", 15), bg="#034033", fg="#F8F8F8")
+    btn_ventas = tk.Button(root, text="Ventas", width=25, command=lambda: open_sales_crud(root, open_main_window), font=("Arial", 15), bg="#034033", fg="#F8F8F8",activebackground= "#022f25", activeforeground="#eeeeee")
     btn_ventas.pack(pady=5)
 
-    btn_informes = tk.Button(root, text="Informes", width=25, font=("Arial", 15), bg="#034033", fg="#F8F8F8")
+    btn_informes = tk.Button(root, text="Informes", width=25, font=("Arial", 15), bg="#034033", fg="#F8F8F8",activebackground= "#022f25", activeforeground="#eeeeee")
     btn_informes.pack(pady=5)
 
     root.mainloop()
